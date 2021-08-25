bb676c9d3b221b65dcd6f06cd45b3ef230a16a17
Kasparas Anusauskas <k.anusauskas@spectrofinance.lt>
2021-08-25 12:23:08 +0300

    Matterjs experiment update

--- ongoingExperiments/matter-js-dom/working_dom_first_index.js
+++ ongoingExperiments/matter-js-dom/working_dom_first_index.js
@@ -2,18 +2,16 @@
 var Engine = Matter.Engine,
   Runner = Matter.Runner,
   Bodies = Matter.Bodies,
-  Composite = Matter.Composite,
-  Render = Matter.Render,
-  Body = Matter.Body;
-
-const FORCE = 0.001;
+  Composite = Matter.Composite;
 
 // create an engine
 var engine = Engine.create();
-engine.world.gravity.y = 0;
 
 var canvas = document.createElement("canvas"),
   context = canvas.getContext("2d");
+
+const boxDimensions = { width: 80, height: 80 };
+const groundDimensions = { width: 810, height: 60 };
 
 // Convert DOM elements to matterjs bodies
 let bodies = [];
@@ -38,35 +36,10 @@ console.log("bodies collected from dom:", bodies);
 
 const matterBodies = bodies.map((body) => Bodies.rectangle(...body));
 
-Matter.Events.on(engine, "beforeUpdate", function (event) {
-  for (i = 0; i < domBlocks.length; ++i) {
-    const domBlock = domBlocks[i];
-    const renderedBody = matterBodies[i];
-    if (domBlock.classList.contains("moveUp")) {
-      Body.applyForce(renderedBody, { x: renderedBody.position.x, y: renderedBody.position.y }, { x: 0, y: -FORCE });
-    }
-    if (domBlock.classList.contains("moveRight")) {
-      Body.applyForce(renderedBody, { x: renderedBody.position.x, y: renderedBody.position.y }, { x: FORCE, y: 0 });
-    }
-    if (domBlock.classList.contains("moveBottom")) {
-      Body.applyForce(renderedBody, { x: renderedBody.position.x, y: renderedBody.position.y }, { x: 0, y: FORCE });
-    }
-    if (domBlock.classList.contains("moveLeft")) {
-      Body.applyForce(renderedBody, { x: renderedBody.position.x, y: renderedBody.position.y }, { x: -FORCE, y: 0 });
-    }
-  }
-});
-
 // add all of the bodies to the world
 Composite.add(engine.world, [...matterBodies]);
 
-var render = Render.create({
-  element: document.body,
-  engine: engine,
-});
-
-function renderDOM() {
-  // During render I have to check if bodies are toggling power ON / OFF
+function render() {
   var renderedBodies = Composite.allBodies(engine.world);
   //   console.log(renderedBodies);
   for (var i = 0; i < renderedBodies.length; i += 1) {
@@ -79,12 +52,8 @@ function renderDOM() {
 }
 
 setInterval(() => {
-  //   console.log(matterBodies[0].position.x);
-  renderDOM();
-}, 10);
-
-// run the renderer
-Render.run(render);
+  render();
+}, 50);
 
 // create runner
 var runner = Runner.create();
