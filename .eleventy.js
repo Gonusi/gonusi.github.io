41664f4a8d9a9b58ef770bb3a10d9f42ed25ee05
Kasparas Anusauskas <kasparasanusauskas@Kasparass-MacBook-Pro.local>
2021-10-02 20:51:44 +0300

    Add basic favicon

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPlugin(syntaxHighlight);
 	eleventyConfig.addPassthroughCopy("css");
+	eleventyConfig.addPassthroughCopy("assets");
+
 
 	return {
 		passthroughFileCopy: true,
